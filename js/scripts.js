2cd874f56559c953177ce8e10dbd04e4c7183a25
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-03-28 13:01:51 +0200

    Add basic files html, scss, js, json, md and .gitignore

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// scripts.js file
+var name = prompt('Enter your name');
+alert('Hello, ' + name + '.');
+console.log('Hello, ' + name + '.');
+var age = prompt ('Could you please state your age, ' + name + '? Please enter a numerical value, for example, 22.');
+alert('You are ' + age + ' years old, ' + name + '.');
+console.log ('You are ' + age + ' years old, ' + name + '.');
+var country = prompt('Please tell us in which country do you live, ' + name + '.');
+alert(country + ' is a very nice place to live in, ' + name + '. Specially if you are ' + age + ' years old:).');
+console.log(country + ' is a very nice place to live in, ' + name + '. Specially if you are ' + age + ' years old:).');
